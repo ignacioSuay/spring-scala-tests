5383f3347e77b339f937ed23d4fcf38835286d0a
44087895 <ignacio.s.mas@noexternalmail.hsbc.com>
2017-10-27 16:31:45 +0100

    create project

---
+++ src/main/java/com/ignaciosuay/springscalatests/SpringScalaTestsApplication.java
@@ -0,0 +1,12 @@
+package com.ignaciosuay.springscalatests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScalaTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringScalaTestsApplication.class, args);
+	}
+}
