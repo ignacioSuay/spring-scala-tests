fed301cdb0b04bf998693c832d873932f39ab487
44087895 <ignacio.s.mas@noexternalmail.hsbc.com>
2017-12-01 17:18:05 +0000

    update hello controller

--- src/main/java/com/ignaciosuay/springscalatests/resource/HelloControler.java
+++ src/main/java/com/ignaciosuay/springscalatests/resource/HelloControler.java
@@ -1,14 +1,19 @@
 package com.ignaciosuay.springscalatests.resource;
 
+import com.ignaciosuay.springscalatests.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloResource {
+public class HelloControler {
+
+    @Autowired
+    HelloService helloService;
 
     @GetMapping("/hello/{name}")
     public String sayHello(@PathVariable String name){
-        return "Hello " + name + "!";
+        return helloService.sayHi(name);
     }
 }
