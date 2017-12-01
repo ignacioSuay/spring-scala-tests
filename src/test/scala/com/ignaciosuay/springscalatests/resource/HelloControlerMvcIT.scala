82d921538a41e33293cc7f63d1edcfbf9892985d
44087895 <ignacio.s.mas@noexternalmail.hsbc.com>
2017-12-01 17:18:55 +0000

    add hello controler mvc it

---
+++ src/test/scala/com/ignaciosuay/springscalatests/resource/HelloControlerMvcIT.scala
@@ -0,0 +1,44 @@
+package com.ignaciosuay.springscalatests.resource
+
+import com.ignaciosuay.springscalatests.service.HelloService
+import org.junit.runner.RunWith
+import org.mockito.Mockito._
+import org.scalatest.{FunSuite, GivenWhenThen}
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.TestContextManager
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.{content, status}
+
+
+@RunWith(classOf[SpringRunner])
+@WebMvcTest(Array(classOf[HelloControler]))
+class HelloControlerMvcIT extends FunSuite with GivenWhenThen {
+
+  @Autowired
+  var mvc: MockMvc = _
+
+  @MockBean
+  val helloService: HelloService = null
+
+  new TestContextManager(this.getClass).prepareTestInstance(this)
+
+  test("test say hello") {
+
+    Given("a name: 'World'")
+    val name = "World"
+    when(helloService.sayHi(name)).thenReturn("Hello World!")
+
+    When("a request to /hello/{name} is sent")
+    val result = mvc.perform(get(s"/hello/$name").contentType("application/json"))
+
+    Then("get hello world!")
+    result
+      .andExpect(status.isOk)
+      .andExpect(content().string("Hello World!"))
+  }
+
+}
