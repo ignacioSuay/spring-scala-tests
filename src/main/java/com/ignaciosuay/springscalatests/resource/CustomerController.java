44e7f848fcd1861a70f092eba13c9794aba9c436
44087895 <ignacio.s.mas@noexternalmail.hsbc.com>
2018-01-26 16:11:59 +0000

    add logging information

--- src/main/java/com/ignaciosuay/springscalatests/resource/CustomerController.java
+++ src/main/java/com/ignaciosuay/springscalatests/resource/CustomerController.java
@@ -3,6 +3,7 @@ package com.ignaciosuay.springscalatests.resource;
 import com.ignaciosuay.springscalatests.model.Customer;
 import com.ignaciosuay.springscalatests.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,12 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class CustomerController {
 
     private final CustomerService customerService;
 
     @GetMapping("/customers/{id}")
     public Customer getCustomer(@PathVariable Long id){
+        log.info("Get customer by id {}", id);
         return customerService.findCustomer(id);
     }
 }
